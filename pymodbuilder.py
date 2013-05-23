fca4b6e18704ffe4b250610ae830f74fdf9163d1
dooz <thofsommer@gmail.com>
2013-05-23 14:38:43 -0500

    Update pymodbuilder.py
    added modules, django, pylockfile, numexpr, bottleneck, pandas

--- pymodbuilder.py
+++ pymodbuilder.py
@@ -20,8 +20,11 @@ tools['git']='git clone -q {url} {path}'
 tools['svn']='svn co -q --non-interactive {url} {path}'
 default_commands=[
                   'python setup.py bdist_wininst',
-                  lambda name: rmtree( pjoin(buildhome ,name) ),
-                  lambda name: shutil.copytree(  pjoin( home,name,'dist'), pjoin(buildhome,name)  )
+                  lambda name: rmtree( pjoin(buildhome, name) ),
+                  lambda name: shutil.copytree(  pjoin( home,name,'dist'), pjoin(buildhome,name)  ),
+                  lambda nada: chdir(home),
+                  lambda name: rmtree_q( pjoin(home, name,'.git') ),
+                  lambda name: rmtree_q( pjoin(home, name) )
                  ]
 ipython_commands=default_commands.copy()
 ipython_commands.insert(0,'git submodule init')
@@ -32,15 +35,16 @@ regex_commands.insert(0,lambda name: shutil.copytree( pjoin(home,name,'docs'), p
 regex_commands.insert(1,lambda name: shutil.copytree( pjoin(home,name,'regex_3','Python'), pjoin(home,name,'Python3') ))
 regex_files=('_regex.c','_regex.h','_regex_unicode.c','_regex_unicode.h') #whyyy? write a copy directory function with walk()
 regex_copy=lambda name,filename: shutil.copyfile( pjoin(home,name,'regex_3','regex',filename), pjoin(home,name,'Python3',filename) )
-regex_commands.insert(-2, lambda name: regex_copy(name,'_regex.c') )
-regex_commands.insert(-2, lambda name: regex_copy(name,'_regex.h') )
-regex_commands.insert(-2, lambda name: regex_copy(name,'_regex_unicode.c') )
-regex_commands.insert(-2, lambda name: regex_copy(name,'_regex_unicode.h') )
-regex_commands.insert(-2,'python PyPI\setup.py bdist_wininst')
+regex_commands.insert((1-len(default_commands)), lambda name: regex_copy(name,'_regex.c') )
+regex_commands.insert((1-len(default_commands)), lambda name: regex_copy(name,'_regex.h') )
+regex_commands.insert((1-len(default_commands)), lambda name: regex_copy(name,'_regex_unicode.c') )
+regex_commands.insert((1-len(default_commands)), lambda name: regex_copy(name,'_regex_unicode.h') )
+regex_commands.insert((1-len(default_commands)),'python PyPI\setup.py bdist_wininst')
 
 modules={}
 #modules['pyzmq']={'directory':'pyzmq', 'tool':tools['git'], 'url':'https://github.com/zeromq/pyzmq.git', 'commands':default_commands}
 #modules['pycares']={'directory':'pycares', 'tool':tools['git'], 'url':'https://github.com/saghul/pycares.git', 'commands':default_commands}
+#these two above ^ i've gotten to build but not automated the process
 modules['pygments']={'directory':'pygments', 'tool':tools['hg'], 'url':'https://bitbucket.org/birkenfeld/pygments-main','commands':default_commands}
 modules['sphinx']={'directory':'sphinx', 'tool':tools['hg'], 'url':'https://bitbucket.org/birkenfeld/sphinx','commands':default_commands}
 modules['nose']={'directory':'nose', 'tool':tools['git'], 'url':'https://github.com/nose-devs/nose.git', 'commands':default_commands}
@@ -54,7 +58,13 @@ modules['tornado']={'directory':'tornado', 'tool':tools['git'], 'url':'https://g
 modules['paramiko']={'directory':'paramiko', 'tool':tools['git'], 'url':'https://github.com/paramiko/paramiko.git', 'commands':default_commands}
 modules['WMI']={'directory':'WMI', 'tool':tools['svn'], 'url':'http://svn.timgolden.me.uk/wmi/trunk/', 'commands':default_commands}
 modules['regex']={'directory':'regex', 'tool':tools['hg'], 'url':'https://code.google.com/p/mrab-regex-hg/', 'commands':regex_commands}
+modules['pylockfile']={'directory':'pylockfile', 'tool':tools['git'], 'url':'https://github.com/smontanaro/pylockfile.git', 'commands':default_commands}
+modules['django']={'directory':'django', 'tool':tools['git'], 'url':'https://github.com/django/django.git', 'commands':default_commands}
+modules['numexpr']={'directory':'numexpr', 'tool':tools['hg'], 'url':'https://code.google.com/p/numexpr/', 'commands':default_commands}
+modules['bottleneck']={'directory':'bottleneck', 'tool':tools['git'], 'url':'https://github.com/kwgoodman/bottleneck.git', 'commands':default_commands}
+modules['pandas']={'directory':'pandas', 'tool':tools['git'], 'url':'https://github.com/pydata/pandas.git', 'commands':default_commands}
 #modules['']={'directory':'', 'tool':tools[''], 'url':'', 'commands':default_commands}
+
 
 for module in modules:
     wm=modules[module]
